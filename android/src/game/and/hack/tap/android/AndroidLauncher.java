08005481fd597f222324b1580955e2da899b5957
Turupawn <ahmed.hn.43@gmail.com>
2015-09-12 15:20:50 -0600

    tap

---
+++ android/src/game/and/hack/tap/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package game.and.hack.tap.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import game.and.hack.tap.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
---
+++ desktop/src/game/and/hack/tap/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package game.and.hack.tap.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import game.and.hack.tap.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
